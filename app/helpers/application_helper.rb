f68ca64b94e1ff43c9fa46474c5e0baf899b7380
vergil <vergil.sm@yandex.ru>
2018-03-14 15:58:14 +0300

    adds item_new to layouts/home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,16 @@ module ApplicationHelper
       asset_path('logo.jpg')
     end
   end
+
+  # Новинки: Три последних добавленных товара
+  def item_new(num)
+    case num
+    when 1
+      Item.order(created_at: :desc)[0]
+    when 2
+      Item.order(created_at: :desc)[1]
+    when 3
+      Item.order(created_at: :desc)[2]
+    end
+  end
 end
