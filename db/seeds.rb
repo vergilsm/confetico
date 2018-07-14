80af1509bb21fdfba0b38d775ebedf9d313dd1da
vergil <vergil.sm@yandex.ru>
2018-07-14 16:08:43 +0300

    remove database.yml and secrets.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(email: Rails.application.secrets.email,
+             password: Rails.application.secrets.password)
