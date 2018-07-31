5fdaece9adcf090aa5c26453a6abbc7960784ee8
vergil <vergil.sm@yandex.ru>
2018-07-31 14:08:12 +0300

    changed REGEXP in the models/order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
-  REGEXP_USER_NAME = /[\u0400-\u04FF\А-\Я|\а-\я]*/i
+  REGEXP_USER_NAME = /[\u0400-\u04FF\А-\Я|\а-\я]+\z/
   REGEXP_PHONE = /(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}/
   REGEXP_EMAIL = /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
@@ -9,6 +9,7 @@ class Order < ApplicationRecord
   validates :user_name, format: {with: REGEXP_USER_NAME}
   validates :phone, format: {with: REGEXP_PHONE}
   validates :email, format: {with: REGEXP_EMAIL}
+  validates :user_name, length: {maximum: 30}
   validates :address, length: {maximum: 150}
 
   after_create :minus_quantity_item
